326d50fd621cdcc000bb9e52bdab444cee27ef8e
Evseev Anton <soki1997@mail.ru>
2022-08-31 20:41:49 +0500

    hw3 initial type

--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -1,17 +1,18 @@
 import React, {useState} from 'react'
 import GreetingContainer from './GreetingContainer'
+import {v1} from 'uuid/index';
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string // Done!
+    name: string // Done!
 }
 
 // уровень работы с глобальными данными
 function HW3() {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<any>(['']) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
+    const addUserCallback = (name: string) => { // need to fix any
         setUsers([]) // need to fix
     }
 
